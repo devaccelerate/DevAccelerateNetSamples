e9b35b47d115eda9c1c48387ba5f75b797ff8dcd
Tanveer Y <tanveery@users.noreply.github.com>
2019-12-23 13:36:15 +0530

    Updated the echo api sample

---
+++ src/AspNetWebApi/Controllers/EchoApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ejyle.DevAccelerate.Samples.AspNetWebApi.Controllers
+{
+    public class EchoApiController : ApiController
+    {
+        public string Get(string message)
+        {
+            var headers = Request.Headers;
+            string result = $"Message: {message} | Headers: ";
+
+            if(headers != null)
+            {
+                foreach(var header in headers)
+                {
+                    result = result + $"{header.Key} = {header.Value.SingleOrDefault()}";
+                }
+            }
+
+            return result;
+        }
+    }
+}
